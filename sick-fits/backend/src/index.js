e9e6f2764533a958e6414d09c8b79ae19cb571c4
Steph <N/A>
2018-11-28 23:04:51 -0500

    Setup GQL Yoga and faked query and mutations

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,18 @@
-// let's go!
+require('dotenv').config({ path: '.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO ::
+// handle cookies (JWT) and populate user
+// with Express middleware
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  }
+}, details => {
+  console.log(`Server is running on port http://localhost:${details.port}`)
+})
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || []
+    const newDog = { name: args.name }
+    console.log(args)
+    global.dogs.push(newDog)
+    return newDog
+  }
+}
 
-module.exports = mutations;
+module.exports = Mutations
