9c626b54dc45e396b8a0b0e4c39ea567492491bc
Steph <N/A>
2018-12-02 22:16:29 -0500

    Setup Item - mutations and query (forwardTo)

--- slick-threads/backend/src/resolvers/Mutation.js
+++ slick-threads/backend/src/resolvers/Mutation.js
@@ -1,10 +1,22 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    const newDog = { name: args.name }
-    console.log(args)
-    global.dogs.push(newDog)
-    return newDog
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   const newDog = { name: args.name }
+  //   console.log(args)
+  //   global.dogs.push(newDog)
+  //   return newDog
+  // }
+
+  async createItem(parent, args, ctx, info) {
+    // TODO:: Check if user is logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    )
+
+    return item
   }
 }
 
--- slick-threads/backend/src/resolvers/Query.js
+++ slick-threads/backend/src/resolvers/Query.js
@@ -1,8 +1,18 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    return global.dogs
-  }
+  // dogs(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || []
+  //   return global.dogs
+  // }
+
+  // For not custom operations
+  items: forwardTo('db'),
+
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // }
 }
 
 module.exports = Query
